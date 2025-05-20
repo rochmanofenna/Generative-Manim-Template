f00e1075554cb5ba0d243398c7b398575f3d706c
penguin <penguin@hiseas.im>
2025-05-20 11:23:39 +0800

    docker deploy

---
+++ service.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+IMAGE_TAG=latest
+if [ -n "$1" ]; then
+IMAGE_TAG="$1"
+fi
+echo "IMAGE_TAG:$IMAGE_TAG"
+
+DOCKER_PULL_REGISTRY=${DOCKER_PULL_REGISTRY:-10.168.0.2:5000}
+IMAGE_NAME=cfa/agent-manim
+SERVICE_NAME="cfa-manim-service"
+
+# pull image
+echo "docker pull ${DOCKER_PULL_REGISTRY}/${IMAGE_NAME}:$IMAGE_TAG"
+docker pull ${DOCKER_PULL_REGISTRY}/${IMAGE_NAME}:$IMAGE_TAG
+if [ $? -eq 0 ]; then
+    echo "succeed"
+else
+    echo "error failed ！！！"
+    exit 1
+fi
+
+echo "docker image pull done"
+
+# stop old container
+if docker ps -a --format '{{.Names}}' | grep -Eq "^${SERVICE_NAME}\$"; then
+    docker stop $SERVICE_NAME
+    docker rm $SERVICE_NAME
+fi
+
+# docker run
+echo "docker run..."
+docker run --add-host=host.docker.internal:host-gateway -d --restart unless-stopped --name $SERVICE_NAME \
+        -v $DOCKER_VOLUME_SRC:$DOCKER_VOLUME_DST \
+	-p $HOST_PORT:$DOCKER_PORT \
+        ${DOCKER_PULL_REGISTRY}/$IMAGE_NAME:$IMAGE_TAG
+if [ $? -eq 0 ]; then
+    echo "succeed"
+else
+    echo "error failed ！！！"
+    exit 1
+fi
+echo "docker run done"
